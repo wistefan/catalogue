10806e99b9815076739e078f9f862f679d687ead
Stefan Wiedemann <wistefan@googlemail.com>
2022-01-18 14:40:13 +0100

    try diffrent clair script

--- .github/container-scan.sh
+++ .github/container-scan.sh
@@ -1,160 +1,87 @@
 #!/bin/bash
+set -ueo pipefail
 
-user="<username>"
-password="<password>"
+log_error() { echo -e "\033[0m\033[1;91m${*}\033[0m"; }
+log_success() { echo -e "\033[0m\033[1;92m${*}\033[0m"; }
 
-usage() {
-    echo "Usage: $0 [-pv] [IMAGE_NAME]"
-    echo
-    echo "Options:"
-    echo " -p : Pull images before running scan"
-    echo " -v : Verbose output"
-    echo " -h : This help message"
-    echo
-    echo "[IMAGE_NAME] : (Optional) Docker image file to be analysed."
-    echo "               If it is not provided the Docker images are "
-    echo "               obtained from the enablers.json file."
-    exit 1
-}
+image=${1:-}
 
-redirect_stderr() {
-    if [[ ${VERBOSE} -eq 1 ]]; then
-        "$@"
-    else
-        "$@" 2>/dev/null
-    fi
-}
-
-redirect_all() {
-    if [[ ${VERBOSE} -eq 1 ]]; then
-        "$@"
-    else
-        "$@" 2>/dev/null >/dev/null
-    fi
-}
-
-security_analysis() {
-    redirect_all echo "Pulling from "$@"..."
-    redirect_all docker pull "$@"
-    redirect_all echo
-
-    labels=$(docker inspect --type=image "$@" 2>/dev/null | jq .[].Config.Labels)
-
-    if [[ ${PULL} -eq 1 ]];
-    then
-      redirect_all echo "Pulling Clair content ..."
-      redirect_all docker-compose pull
-      redirect_all echo
-    fi
-
-    redirect_all echo "Security analysis of "$@" image..."
-    extension="$(date +%Y%m%d_%H%M%S).json"
-    filename=$(echo "$@" | awk -F '/' -v a="$extension" '{print $2 a}')
-    enabler=$(echo "$@" | awk -F '/' '{print $2}')
-
-    redirect_stderr docker-compose run --rm scanner "$@" > ${filename}
-    ret=$?
-    redirect_all echo
-
-    redirect_all echo "Removing docker instances..."
-    redirect_all docker-compose down
-    redirect_all echo
-
-    line=$(grep 'latest: Pulling from arminc\/clair-db' ${filename})
-
-    # Just for the 1st time...
-    if [[ -n ${line} ]]; then
-	    # Delete first 3 lines of the file due to the first time that it is executed
-	    # it includes 3 extra no needed lines
-	    sed -i '1,3 d' ${filename}
-    fi
-
-    # Just to finish, send the data to the nexus instance
-    redirect_all curl -v -u ${user}':'${password} --upload-file ${filename}  https://nexus.lab.fiware.org/repository/security/check/${enabler}/cve/${filename}
-
-    # Send an email to the owner of the FIWARE GE
-
-}
-
-docker_bench_security() {
-    cd ../docker-bench-security
-
-    id=$(docker images | grep -E "$@" | awk -e '{print $3}')
-
-    redirect_all ./docker-bench-security.sh  -t "$@" -c container_images,container_runtime,docker_security_operations
-
-    extension="$(date +%Y%m%d_%H%M%S).json"
-    filename=$(echo "$@" | awk -F '/' -v a="$extension" '{print $2 a}')
-    enabler=$(echo "$@" | awk -F '/' '{print $2}')
-
-    mv docker-bench-security.sh.log.json ${filename}
-
-    redirect_all echo "Clean up the docker image..."
-    redirect_all docker rmi ${id}
-    redirect_all echo
-
-    redirect_all curl -v -u ${user}':'${password} --upload-file ${filename}  https://nexus.lab.fiware.org/repository/security/check/${enabler}/bench-security/${filename}
-
-    cd ../clair-container-scan
-}
-
-init() {
-    BASEDIR=$(cd $(dirname "$0") && pwd)
-    cd "$BASEDIR"
-
-    if [[ ! -f "docker-compose.yml" ]]; then
-        wget -q https://raw.githubusercontent.com/flopezag/fiware-clair/develop/docker/docker-compose.yml
-    fi
-
-    if [[ ! -f "enablers.json" ]]; then
-        wget -q https://raw.githubusercontent.com/flopezag/fiware-clair/develop/docker/enablers.json
-    fi
-
-    cd ..
-
-    if [[ ! -d "docker-bench-security" ]]; then
-        redirect_all git clone https://github.com/docker/docker-bench-security.git
-    fi
-
-    echo $BASEDIR
-    cd "$BASEDIR"
-}
-
-PULL=0
-VERBOSE=0
-
-while getopts ":phv" opt; do
-    case ${opt} in
-        p)
-            PULL=1
-            ;;
-        v)
-            VERBOSE=1
-            ;;
-        \?)
-            echo "Invalid option: -$OPTARG" >&2
-            usage
-            ;;
-        h)
-            usage
-            ;;
-    esac
-done
-shift $(($OPTIND -1))
-
-init
-
-if [[ -n $1 ]]; then
-    security_analysis "$1"
-    docker_bench_security "$1"
-else
-    for ge in `more enablers.json | jq .enablers[].image | sed 's/"//g'`
-    do
-      security_analysis ${ge}
-      docker_bench_security ${ge}
-      redirect_all echo
-      redirect_all echo
-    done
+if [[ -z "${image:-}" ]] || ! [[ "${image:-}" =~ : ]]; then
+  log_error "must specify image:version to scan"
+  exit 1
 fi
 
-exit ${ret}
+function scanner_docker_build() {
+  echo $'
+    FROM amazonlinux:2
+    RUN curl -L \
+        https://github.com/arminc/clair-scanner/releases/download/v12/clair-scanner_linux_amd64 \
+        -o clair-scanner && \
+        chmod +x ./clair-scanner
+  ' | docker build -t clair-scanner -
+}
+
+if ! scanner_docker_build >/dev/null 2>&1; then
+  scanner_docker_build
+fi
+
+function scanner_docker_compose() {
+  echo $'
+    version: "3"
+    services:
+      postgres:
+        image: arminc/clair-db:latest
+        networks:
+          - clair-local
+        restart: always
+      clair:
+        image: arminc/clair-local-scan:latest
+        networks:
+          - clair-local
+        restart: always
+    networks:
+      clair-local:
+        driver: bridge
+  ' | docker-compose -f - "${@}"
+}
+
+if ! scanner_docker_compose pull >/dev/null 2>&1; then
+  scanner_docker_compose pull
+fi
+
+if ! scanner_docker_compose up -d >/dev/null 2>&1; then
+  scanner_docker_compose up -d
+fi
+
+report_file=$(echo "${image:?}.json" | sed  's/\W/-/g')
+
+trap "scanner_docker_compose down > /dev/null 2>&1" int exit
+
+reports=$(mktemp -d)
+
+function scan() {
+  local image="${1:?}"
+  docker network list
+  docker pull $image
+  docker run \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v "${reports:?}:/reports" \
+    --rm \
+    --network=fiware-security_clair-local \
+    clair-scanner \
+    bash -c """
+     while ! curl -q http://clair:6060 > /dev/null 2>&1; do
+       sleep 1
+     done
+      ./clair-scanner --ip \${HOSTNAME:?} -r /reports/${report_file:?} --clair http://clair:6060 \"${image:?}\"
+    """
+}
+
+if scan "${image:?}"; then
+  log_success "✨ ${image:?} contains no known vulnerabilities. ✨"
+else
+  cat  ${reports:?}/${report_file:?}
+
+  log_error "😱 ${image:?} contains vulnerabilities, details saved to ${reports:?}/${report_file:?}. 😱"
+  exit 1
+fi
